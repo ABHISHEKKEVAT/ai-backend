a377137061848551ed657fddef2779f4658ddefe
AbhishekKevat <kevatshivam16@gmail.com>
2026-03-18 14:57:06 +0100

    update

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
-require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
+const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 
-const app = express();
+dotenv.config();
 connectDB();
 
-app.use(cors());
+const app = express();
+
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
-app.use("/uploads", express.static("uploads"));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/users", require("./routes/user"));
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
