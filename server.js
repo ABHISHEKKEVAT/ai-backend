e38b785e64967e1ef4db7c9940c1525f85cf342b
AbhishekKevat <kevatshivam16@gmail.com>
2026-03-18 23:58:01 +0100

    update

--- server.js
+++ server.js
@@ -17,4 +17,7 @@ app.use("/api/auth", require("./routes/auth"));
 app.use("/api/users", require("./routes/user"));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
